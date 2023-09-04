7cf71fb3ed0ca8f72b190cb68b7ff48161e1df95
lab900-sander-van-loock <sander.vanloock@lab900.com>
2023-09-04 08:45:41 +0200

    functions working

--- integration-paymo/src/index.ts
+++ integration-paymo/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const integrationPaymo = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Integration Paymo!");
 });
--- notifications/src/index.ts
+++ notifications/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const notifications = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
+    response.send("Hello from Notifications!");
 });
