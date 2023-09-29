4befa17383b040d096dd43729d57e1090bf36b8e
Gino Van Houdt <gino.vanhoudt@lab900.com>
2023-09-29 16:06:14 +0200

    added table component

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import HolidappTable from "../components/holidapp.table";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  const headers = [
+    {
+      key: "requester",
+      header: "Requester",
+    },
+    {
+      key: "from",
+      header: "From",
+    },
+    {
+      key: "to",
+      header: "To",
+    },
+    {
+      key: "status",
+      header: "Status",
+    },
+    {
+      key: "amountOfDaysTaken",
+      header: "Amount of days taken",
+    },
+  ];
+  return (
+    <>
+      <h1>Home</h1>
+      <HolidappTable headers={headers} />
+    </>
+  );
 }
---
+++ frontend/src/components/holidapp.table.tsx
@@ -0,0 +1,71 @@
+import {
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+} from "@carbon/react";
+
+export interface HolidappTableProps {
+  headers: {
+    key: string;
+    header: string;
+  }[];
+}
+export default function HolidappTable(props: HolidappTableProps) {
+  const mockDataRows = [
+    {
+      id: "a",
+      requester: "Geralt of Rivia",
+      from: "15/10/2023",
+      to: "27/12/2023",
+      status: "Approved",
+      amountOfDaysTaken: "2",
+    },
+    {
+      id: "b",
+      requester: "Clark Kent",
+      from: "15/10/2023",
+      to: "27/12/2023",
+      status: "Approved",
+      amountOfDaysTaken: "6",
+    },
+    {
+      id: "c",
+      requester: "Johny Bravo",
+      from: "15/10/2023",
+      to: "27/12/2023",
+      status: "Approved",
+      amountOfDaysTaken: "1",
+    },
+  ];
+
+  return (
+    <DataTable rows={mockDataRows} headers={props.headers}>
+      {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+        <Table {...getTableProps()}>
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <TableHeader {...(getHeaderProps({ header }) as any)}>
+                  {header.header}
+                </TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow {...getRowProps({ row })}>
+                {row.cells.map((cell) => (
+                  <TableCell key={cell.id}>{cell.value}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </DataTable>
+  );
+}
