3afece1a7d317458879858522d938b67d5e57bd3
Frederico <frederico.teixeira@lab900.com>
2023-09-29 17:56:41 +0200

    feat: add AddHolidayRequest component

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.scss';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
-import {Auth} from "../components/Auth";
+import { Auth } from "../components/Auth";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Auth />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Auth />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
