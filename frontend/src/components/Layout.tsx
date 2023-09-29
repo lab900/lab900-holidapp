18700abca31136dd138e82ec72aa52ae240a8a5e
Frederico <frederico.teixeira@lab900.com>
2023-09-29 15:37:45 +0200

    chore: apply prettier to Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,23 @@
-import {Header, HeaderMenuItem, HeaderName, HeaderNavigation} from "@carbon/react";
+import {
+  Header,
+  HeaderMenuItem,
+  HeaderName,
+  HeaderNavigation,
+} from "@carbon/react";
 
 export default function Layout() {
-    return (
-        <div className="Layout">
-            <Header aria-label="Lab900 - Holidays">
-                <HeaderName href="#" prefix="Lab900">Holidays</HeaderName>
-                <HeaderNavigation aria-label="IBM [Platform]">
-                    <HeaderMenuItem href="#">My request</HeaderMenuItem>
-                    <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-                    <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-                </HeaderNavigation>
-            </Header>
-        </div>
-    );
+  return (
+    <div className="Layout">
+      <Header aria-label="Lab900 - Holidays">
+        <HeaderName href="#" prefix="Lab900">
+          Holidays
+        </HeaderName>
+        <HeaderNavigation aria-label="IBM [Platform]">
+          <HeaderMenuItem href="#">My request</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+        </HeaderNavigation>
+      </Header>
+    </div>
+  );
 }
