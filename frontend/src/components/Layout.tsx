aca828df19db7e4790e1a49aadabdc08dbab0ca3
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2023-09-29 15:59:58 +0200

    Added carbon icons

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import {
   Content,
-  Header,
+  Header, HeaderGlobalAction, HeaderGlobalBar,
   HeaderMenuItem,
   HeaderName,
   HeaderNavigation,
 } from "@carbon/react";
 import { Link, Outlet } from "react-router-dom";
+import React from "react";
+import { Settings, Logout } from '@carbon/icons-react';
 
 export default function Layout() {
   return (
@@ -20,6 +22,14 @@ export default function Layout() {
             <Link to={"/auth"}>Auth</Link>
           </HeaderMenuItem>
         </HeaderNavigation>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Admin">
+            <Settings size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Logout">
+            <Logout size={20} />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
       </Header>
       <Content>
         <Outlet />
