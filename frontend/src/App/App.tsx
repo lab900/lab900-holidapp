f5d30b3c9f100483166a9360f52029247c04952d
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2023-09-29 15:47:08 +0200

    Update page structure

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.scss';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
-import {Auth} from "../components/Auth";
+import { Auth } from "../Pages/Auth";
+import Home from "../Pages/Home";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Auth />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="auth" element={<Auth />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,23 +1,29 @@
 import {
+  Content,
   Header,
   HeaderMenuItem,
   HeaderName,
   HeaderNavigation,
 } from "@carbon/react";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="Layout">
+    <>
       <Header aria-label="Lab900 - Holidays">
-        <HeaderName href="#" prefix="Lab900">
-          Holidays
-        </HeaderName>
+        <HeaderName prefix="Lab900">Holidays</HeaderName>
         <HeaderNavigation aria-label="IBM [Platform]">
-          <HeaderMenuItem href="#">My request</HeaderMenuItem>
-          <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-          <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+          <HeaderMenuItem>
+            <Link to={"/"}>My request</Link>
+          </HeaderMenuItem>
+          <HeaderMenuItem>
+            <Link to={"/auth"}>Auth</Link>
+          </HeaderMenuItem>
         </HeaderNavigation>
       </Header>
-    </div>
+      <Content>
+        <Outlet />
+      </Content>
+    </>
   );
 }
