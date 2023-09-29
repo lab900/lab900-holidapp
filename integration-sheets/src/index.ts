a8ec9749649e904624c6b832aeab61e8aaca4e9d
lab900-johan-cosemans <johan.cosemans@lab900.com>
2023-09-29 16:32:42 +0200

    Added pubsub

--- integration-sheets/src/index.ts
+++ integration-sheets/src/index.ts
@@ -1,10 +1,26 @@
 import * as functions from "firebase-functions";
+import {PubSub} from "@google-cloud/pubsub";
 
+const PUB_SUB_DATA_EXPORT_TOPIC = "holidapp-updates";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const integrationSheets =
     functions.https.onRequest((request, response) => {
-        functions.logger.info("Hello logs!", {structuredData: true});
-        response.send("Hello from Integration Sheets!");
+      functions.logger.info("sending...!", {structuredData: true});
+      const topic = new PubSub().topic(PUB_SUB_DATA_EXPORT_TOPIC);
+      const json = {foo: "bar"};
+      topic.publishMessage({
+        json,
+      });
+      response.send("Message sent 4!");
+    });
+
+export const startSheetIntegration = functions
+    .region("us-central1")
+    .runWith({})
+    .pubsub.topic(PUB_SUB_DATA_EXPORT_TOPIC)
+    .onPublish(async (message) => {
+      functions.logger.info("received:" + message, {structuredData: true});
+      //        await databaseExport(sheetId);
     });
