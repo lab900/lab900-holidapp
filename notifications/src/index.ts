9d4f0449a4695256484922268b9a32067ec69be6
lab90-virginie-slagmolen <virginie.slagmolen@lab900.com>
2023-09-29 17:32:42 +0200

    Integration with Slack and PubSub

--- notifications/src/index.ts
+++ notifications/src/index.ts
@@ -3,8 +3,21 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+const MY_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const SlackNotify = require("slack-notify");
+const slack = SlackNotify(MY_SLACK_WEBHOOK_URL);
+const PUB_SUB_DATA_EXPORT_TOPIC = "holidapp-updates";
 export const notifications =
-    functions.https.onRequest((request, response) => {
-        functions.logger.info("Hello logs!", {structuredData: true});
-        response.send("Hello from Notifications!");
-    });
+    functions.region("us-central1")
+        .runWith({})
+        .pubsub.topic(PUB_SUB_DATA_EXPORT_TOPIC)
+        .onPublish(async (message) => {
+          functions.logger.info("received:" + message, {structuredData: true});
+          slack.send({
+            channel: "#holidapp",
+            text: "Here is my notification",
+            username: "holidapp",
+            icon_url: "https://storage.googleapis.com/com-huapii-lab900-data/assets/logo-1024x883.png",
+          });
+        });
