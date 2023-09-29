c6fb950169d4f20b9f717ba122e30edcdf6fef18
Luis Santos <luis.santos@lab900.com>
2023-09-29 16:30:20 +0200

    feat: create request

--- holidapp-api/src/handlers/create-request.ts
+++ holidapp-api/src/handlers/create-request.ts
@@ -1,11 +1,28 @@
 import { RequestHandler } from "express";
 import * as admin from "firebase-admin";
 import { z } from "zod";
+import { RequestWithUser } from "../middleware/validate-token";
+import { Request } from "../models/Request";
+
+const CreateRequestSchema = z.object({
+  from: z.string().datetime(),
+  to: z.string().datetime(),
+  halfDays: z.array(z.string().datetime()),
+  requesterRemark: z.string().optional(),
+});
 
 export const createRequest: (db: admin.firestore.Firestore) => RequestHandler =
-  (db) => async (req, res) => {
+  (db) => async (req: RequestWithUser, res) => {
     try {
-      const requestData = req.body;
+      const request = CreateRequestSchema.parse(req.body);
+
+      const requestData: Request = {
+        ...request,
+        createdOn: new Date(),
+        requester: req.user!.email,
+        days: 0,
+      };
+
       const docRef = await db.collection("requests").add(requestData);
       res.status(201).send({ id: docRef.id });
     } catch (error) {
