e50ecab2b3d026d8ae250979d436518cb60e353c
lab900-johan-cosemans <johan.cosemans@lab900.com>
2023-09-29 18:08:08 +0200

    merged sheets code

--- integration-sheets/src/index.ts
+++ integration-sheets/src/index.ts
@@ -1,9 +1,20 @@
 import * as functions from "firebase-functions";
 import {PubSub} from "@google-cloud/pubsub";
-import {google} from "googleapis";
+import {google, sheets_v4} from "googleapis";
 
 const PUB_SUB_DATA_EXPORT_TOPIC = "holidapp-updates";
 const OFFICE_SHEET = "1aZ9tHcpQiqDbCmC0lowaRqMUm00oA5bCOMsBxFEOqAU";
+
+
+const auth = new google.auth.JWT({
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  // eslint-disable-next-line max-len
+  key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDsoLz/+4vfPJEE\nlsiAa2/tHp67wjIQunKldHoVisIkO5wIPgefrwDmEWDiZiMWr/75i3PHMGCjnBLR\nLv4mSaSaKiIRA/TregUNVqU6PNWhADeRzDJV/M+RSSwcGVB/qk/HoOI7rlpnfJ+1\nfj07bymrVvfBsG6jvMSjWWT8vcv6j1lb6unvc4c1jL0krIVHwg5KTitrg1DokMQ9\nYzGllP6mFlhZdZcDx3M6gwICCrzc08a2h2stBQIJLNs/Ij/Csg0Vp2Y07bgJOOtD\nOkpPy6nh5HqI41G8zdo6BHUL5is3mvVQTUVDnZPbmPT+CvcNRiSninLaqOanIxfW\nm8TXXiqTAgMBAAECggEAHUvhWogYTzE+eyT3+f5TQzZRTMnETRHoc9ex7czaMIY4\n0URsyTZvlAx+xEkE+yAbH2ZGlVjF/juqkL+Ss8bWXEfvI9ArmKPo0Df0CPvTLzXJ\nq4FukfrU8BXxIiF4N/YsHnkqyVzS0JE7bMsOZbdjn1ULI63mZNtQNTEyMaCGCqVL\n5naYYYIThGULKkoAaB6a1kXJgnDSlYiukBLsNId54R50EGjfuUVdNTWY5HKnClj+\nLKhoppo7S6zWIFTWbUOffMU35JdEEPo9cIVUNPGpQ3Giw3/p9PnWv1nbfEOvSj+/\nhsgDVMKHHCz2427zdFearwMnNhlTJi05GKZCGNj44QKBgQD4RANMhwbsSUGzA/by\nld1nxNiTwfZeyAuQKclUJHYualycGSN4oPFBKMvwxVo6NuvCfsQ8Yf2L906IOEVI\nGpJLiSBC3VSadtGDOhxPZCcps2FbP8Yw2CvTA9D3ARYB8OCg4bHwsO5CMA/HLZkL\naiUw9T2hley/PPQs2FY+Uk9SmQKBgQDz/+kqb1nU9f4yWBMxU5aw15jPPzcYq54g\nDL1BiVVh+jdlFPDgSlG7W8Uitu4c5eO1isjiOEq62kXAuOc6mn4hk3JbYbiE3Djj\nT4NsXfxEdBWCcbq1V1tNBttAQ0BeWJ2gQzU6zbBFBxfA05rocP5G+Fjc1uwyiUe9\nT/HEYwNOCwKBgQCMQtCiUb8Fi7XEj77a+vRlhf7eK70ne1mdDx6hnR/vxUOhJUq3\nAiaLK6Nc2gLJ1PGJptjzTGFUzFnf9/qca/avGj/Rxv3fbrWbsB8Cc5D0rFWamgM8\n4USRKRzMEB8A5bfi7N/9HdjeKA0h+I74tfVhHYy+V9ezi5pgI7FF8HbEMQKBgQDu\nzlLYc28KXVBS2Zy/ND5SClDG1eoIvT7fw1gfolfsssYwqlQuUM65ftHsbfmSUvWQ\noqqFre6F/kcynkXBN5+nIPZVtciwUBFleFb6YuoZ3TuIFeNUxJDZTJKdzLRdIoHz\nbpPUX6fbh1uaIACG6sATmdDvI3K861tJT8siGH4NjQKBgCGHhH7Ae+eK9qq+OGkU\nt9n8a+LFQuXrO1GwHSNy01Rrf8iypqUaru/FjxZ7A+CIRppZrDIe4JWPovi25XT5\nv530vs9UfwixTFnGRv01hymJn5O3k6xzFXZfqXFkzKUgagWCb9m7nWCQmeR+ht3r\nGFpaKqOA4JvqWxTnTz+9U84p\n-----END PRIVATE KEY-----\n",
+  email: "holidap-runner@lab900-holidapp.iam.gserviceaccount.com",
+  keyId: "44004405bdc8aad8ff17c855ab041b044fa59a62",
+});
+
+
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -34,65 +45,88 @@ export const startSheetIntegration = functions
       //        await databaseExport(sheetId);
     });
 
-// eslint-disable-next-line require-jsdoc
-export default async function getData(): Promise<any> {
-  const auth = new google.auth.JWT({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    // eslint-disable-next-line max-len
-    key: "",
-    email: "holidap-runner@lab900-holidapp.iam.gserviceaccount.com",
-    keyId: "44004405bdc8aad8ff17c855ab041b044fa59a62",
-  });
+const openSheet = async (sheetId: string, holidayRequest: Request) => {
   const sheets = google.sheets({version: "v4", auth});
+  // eslint-disable-next-line max-len
+  const worksheetNames = getMonthYearNamesForDates(new Date(holidayRequest.from), new Date(holidayRequest.to));
+
+  for (const worksheetName of worksheetNames) {
+    const worksheetData = await openWorkSheet(sheets, worksheetName);
+    if (worksheetData == null) {
+      continue;
+    }
+    const employeeRow = findEmployeeRow(worksheetData, holidayRequest.requester);
+
+    // Get the columns for the dates between from and to
+  }
+
+
+  // for each month between from and to, do the update:
+  // get the worksheet for the month
+  // get the row for the employee
+  // get the columns for the dates between from and to
+  // eslint-disable-next-line max-len
+  // update the values for the columns to HOL / blank / ... based on the holiday request status
+};
+
+const getMonthYearNamesForDates = (from: Date, to: Date): string[] => {
+  const monthYearNames = [];
+  const current = new Date(from);
+  while (current.getFullYear() <= to.getFullYear() &&
+          current.getMonth() <= to.getMonth()) {
+    // eslint-disable-next-line max-len
+    monthYearNames.push(current.toLocaleString("default", {month: "long"}) + " " + current.getFullYear());
+    current.setMonth(current.getMonth() + 1);
+  }
+  return monthYearNames;
+};
+
+
+// eslint-disable-next-line camelcase,max-len
+const findEmployeeRow = (worksheetData: any[][], employeeEmail: string): number => {
+  const employeeEmailColumnIndex = 1;
+  // eslint-disable-next-line max-len
+  const employeeEmailColumn = worksheetData.map((row) => row[employeeEmailColumnIndex]);
+  return employeeEmailColumn.indexOf(employeeEmail);
+};
+//
+// const findDateColumns = (worksheet, from: date, to: date): int[] => {
+//
+// }
+
+// eslint-disable-next-line camelcase
+const openWorkSheet = async (sheets: sheets_v4.Sheets, worksheetName: string):Promise<any[][] | null | undefined> => {
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: OFFICE_SHEET,
-    range: "September 2023",
+    range: worksheetName,
   });
-  const rows = res.data.values;
-  if (!rows || rows.length === 0) {
-    console.log("No data found.");
-    return;
-  }
-  rows.forEach((row) => {
-    // Print columns A and E, which correspond to indices 0 and 4.
-    console.log(`${row[0]}, ${row[1]}`);
-  });
-  return rows;
-}
+  return res.data.values;
+};
 
-const openSheet = (sheetId: string, holidayRequest: any) => {
-
-    openSheet()
-
-    // for each month between from and to, do the update:
-        // get the worksheet for the month
-        // get the row for the employee
-        // get the columns for the dates between from and to
-        // update the values for the columns to HOL / blank / ... based on the holiday request status
-
-}
-
-const getMonthYearNamesForDates = (from: date, to: date) string[]  => {
-    return ["September 2023", "October 2023"];
-}
+// const getCellValueForHolidayStatus(status: string): string => {
+//     // approved -> HOL
+//     // rejected -> blank
+// }
+//
+//
+// const openWorkSheet = (sheets, worksheetName: string): worksheet => {
+//
+//     // open the google sheet and find the right worksheet based on the date
+//
+// }
 
 
-const findEmployeeRow = (worksheet, employeeEmail): int => {
-
-}
-
-const findDateColumns = (worksheet, from: date, to: date): int[] => {
-
-}
-
-const getCellValueForHolidayStatus(status: string): string => {
-    // approved -> HOL
-    // rejected -> blank
-}
-
-
-const openWorkSheet = (sheets, worksheetName: string): worksheet => {
-
-    // open the google sheet and find the right worksheet based on the date
-
+export interface Request {
+    status: "pending" | "approved" | "rejected",
+    answeredOn?: Date,
+    approver?: string, // User.email
+    requesterRemark?: string
+    approverRemark?: string
+    requester: string // User.email
+    from: string // Date
+    to: string // Date
+    createdOn: Date,
+    days: number,
+    halfDays: string[]
+    year: number
 }
